6809edc99303a1df4e743056fefe66756f85803a
zhang le <zhangle2006@gmail.com>
2024-08-02 09:57:30 +0800

    Build 1.11.0-OpenSSL-1-1-1u

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
     targets: [
         .binaryTarget(name: "CSSH",
                       url: "https://github.com/zhangle/Libssh2Prebuild/releases/download/1.11.0-OpenSSL-1-1-1u/CSSH-1.11.0-OpenSSL-1-1-1u.xcframework.zip",
-                      checksum: "1c3b9fbd6e41f427f3cdfad58a8e96f68ebac0c669afd6b6311328d95e41037c")
+                      checksum: "8aca563a37d19f72c9b0f1c46673793796065215299d2374553e805923408974")
     ]
 )
